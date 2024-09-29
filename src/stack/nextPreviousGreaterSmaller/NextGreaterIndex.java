c6d420ee66c347c94139bed27c82c70418b30a72
aomkar2421 <omkarjagtap1651@gmail.com>
2024-09-29 21:40:31 +0530

    29 Sept

---
+++ src/stack/nextPreviousGreaterSmaller/NextGreaterIndex.java
@@ -0,0 +1,33 @@
+package stack.nextPreviousGreaterSmaller;
+import java.util.Stack;
+import stack.PrintArray;
+
+public class NextGreaterIndex {
+	public static void main(String[] args) {
+		int [] arr = {1,5,2,3,1,6,3,4};
+		PrintArray.print(arr);
+		solution(arr);
+	}
+
+	private static void solution(int[] arr) {
+		Stack<Integer> st = new Stack<Integer>();
+		int n = arr.length;
+		int [] res = new int[n];
+
+		for (int i = n-1; i >= 0; i--) {
+
+			while (st.size()>0 && arr[st.peek()]<arr[i]) {
+				st.pop();
+			}
+			if (st.isEmpty() ) {
+				res[i] = -1;
+			}
+			else if (arr[st.peek()]>arr[i]) {
+				res[i] = arr[st.peek()];
+			}
+			st.push(i);
+		}
+
+		PrintArray.print(res);
+	}
+}
--- src/stack/nextPreviousGreaterSmaller/PreviousSmallerElementIndex.java
+++ src/stack/nextPreviousGreaterSmaller/PreviousSmallerElementIndex.java
@@ -2,7 +2,7 @@ package stack.nextPreviousGreaterSmaller;
 import java.util.Stack;
 import stack.PrintArray;
 
-public class NextGreater {
+public class PreviousSmallerElementIndex {
 	public static void main(String[] args) {
 		int [] arr = {1,5,2,3,1,6,3,4};
 		PrintArray.print(arr);
@@ -14,10 +14,18 @@ public class NextGreater {
 		int n = arr.length;
 		int [] res = new int[n];
 
-		for (int i = n-1; i >= 0; i--) {
-			if (!st.isEmpty() &&  ) {
-				
+		for (int i = 0; i < res.length; i++) {
+
+			while (!st.isEmpty() &&  arr[i]<st.peek()) {
+				st.pop();
 			}
+			if (st.isEmpty()) {
+				res[i] = -1;
+			}
+			else if(!st.isEmpty() && arr[i]>st.peek() ) {
+				res[i] = st.peek();
+			}
+			st.push(arr[i]);
 		}
 
 		PrintArray.print(res);
